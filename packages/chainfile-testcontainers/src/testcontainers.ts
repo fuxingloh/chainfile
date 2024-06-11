b1b61aff2ff5861cdd2fa694f45ac62bebc6ca64
Fuxing Loh <git@fuxing.dev>
2024-06-11 22:56:36 +0800

    refactor

--- packages/chainfile-testcontainers/src/testcontainers.ts
+++ packages/chainfile-testcontainers/src/testcontainers.ts
@@ -4,35 +4,31 @@ import { join } from 'node:path';
 
 import { Compose } from '@chainfile/docker';
 import { Chainfile } from '@chainfile/schema';
-import { DockerComposeEnvironment, StartedDockerComposeEnvironment as ComposeStarted, Wait } from 'testcontainers';
+import { DockerComposeEnvironment, StartedDockerComposeEnvironment as ComposeInstance, Wait } from 'testcontainers';
+import { StartedGenericContainer } from 'testcontainers/build/generic-container/started-generic-container';
 
 import { AgentContainer } from './agent';
 import { ChainfileContainer } from './container';
-
-type Location = Chainfile | string | any;
 
 export class ChainfileTestcontainers {
   protected readonly cwd: string = join(process.cwd(), '.chainfile', 'testcontainers');
   protected readonly suffix = randomBytes(4).toString('hex');
   protected readonly filename = `compose.${this.suffix}.yml`;
+  protected readonly chainfile: Chainfile;
 
-  protected chainfile!: Chainfile;
-  protected compose!: Compose;
-  protected composeStarted!: ComposeStarted;
+  protected composeSynth: Compose;
+  protected composeInstance?: ComposeInstance;
 
-  public constructor(
-    protected readonly location: Location,
-    protected readonly values: Record<string, string> = {},
-  ) {
+  public constructor(chainfile: Chainfile | object, values: Record<string, string> = {}) {
     mkdirSync(this.cwd, { recursive: true });
+    this.composeSynth = new Compose(chainfile as any, values, this.suffix);
+    this.chainfile = chainfile as Chainfile;
   }
 
   public async start(): Promise<void> {
-    this.chainfile = typeof this.location === 'string' ? await fetchChainfile(this.location) : this.location;
-    this.compose = new Compose(this.chainfile, this.values, this.suffix);
-    writeFileSync(join(this.cwd, this.filename), this.compose.synthCompose());
+    writeFileSync(join(this.cwd, this.filename), this.composeSynth.synthCompose());
 
-    const environment = this.compose
+    const environment = this.composeSynth
       .synthDotEnv()
       .split('\n')
       .reduce<Record<string, string>>((acc, line) => {
@@ -40,42 +36,34 @@ export class ChainfileTestcontainers {
         acc[key] = value;
         return acc;
       }, {});
-    this.composeStarted = await new DockerComposeEnvironment(this.cwd, this.filename)
+    this.composeInstance = await new DockerComposeEnvironment(this.cwd, this.filename)
       .withEnvironment(environment)
       // The readiness probe of @chainfile/agent is to determine if the deployment is ready to accept requests.
-      .withWaitStrategy(`agent-${this.compose.suffix}`, Wait.forHttp('/probes/readiness', 1569))
+      .withWaitStrategy(`agent-${this.suffix}`, Wait.forHttp('/probes/readiness', 1569))
       .up();
   }
 
   async stop(): Promise<void> {
-    await this.composeStarted.down();
+    await this.composeInstance?.down();
     rmSync(join(this.cwd, this.filename));
   }
 
   get(name: string): ChainfileContainer {
-    if (this.composeStarted === undefined) {
-      throw new Error('ChainfileTestcontainers not started');
-    }
     const containerDef = this.chainfile.containers[name];
     if (containerDef === undefined) {
       throw new Error(`Container ${name} not found`);
     }
-    return new ChainfileContainer(
-      this.composeStarted.getContainer(`${name}-${this.compose.suffix}`),
-      containerDef,
-      this.compose.values,
-    );
+    return new ChainfileContainer(this.getContainer(name), containerDef, this.composeSynth.values);
   }
 
   getAgent(): AgentContainer {
-    if (this.composeStarted === undefined) {
-      throw new Error('ChainfileTestcontainers not started');
-    }
-    return new AgentContainer(this.composeStarted.getContainer(`agent-${this.compose.suffix}`));
+    return new AgentContainer(this.getContainer(`agent`));
   }
-}
 
-async function fetchChainfile(location: string): Promise<Chainfile> {
-  // Get latest package from npm using pacote
-  throw new Error(`Not implemented: ${location}`);
+  private getContainer(name: string): StartedGenericContainer {
+    if (this.composeInstance === undefined) {
+      throw new Error('Testcontainers not started');
+    }
+    return this.composeInstance.getContainer(`${name}-${this.suffix}`);
+  }
 }
